29d886bc8dabf2792ccee6002d7011490bebb44d
Yue1Harriet1Huang <dariahuangster@gmail.com>
2021-01-06 23:44:47 -0500

    Create env.py

---
+++ env.py
@@ -0,0 +1,18 @@
+"""All of the environment variables
+setters
+getters
+"""
+
+import os
+
+DRIVER = "MY_DB_DRIVER"
+SERVER = "MY_DB_SERVER"
+PORT = "MY_DB_PORT"
+DATABASE = MY_DATABASE""
+UID = "MY_UID"
+PWD = "MY_PWD"
+WINDOWS_AUTHENTICATION = "" #yes or otherwise
+CONN_STR = ""
+CONNECT_METHOD = "" #pyodbc, pyspark, etc.
+
+def set_driver(
