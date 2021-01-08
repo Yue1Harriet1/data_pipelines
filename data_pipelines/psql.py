4c051d37c7fe99eeec0fbaff41da97c8fd6c6464
Yue1Harriet1Huang <dariahuangster@gmail.com>
2021-01-07 21:17:12 -0500

    Update and rename psql.py to data_pipelines/psql.py
    change parent directory

--- data_pipelines/psql.py
+++ data_pipelines/psql.py
@@ -8,3 +8,7 @@ CONNECTION_STRING = env.CONNECTION_STRING
 CONNECTION_STRING = os.getenv(CONNECTION_STRING)
 
 def pyodbc_query(query):
+  cnxn = pyodbc.connect(CONNECTION_STRING)
+  return(pd.read_sql(query, cnxn))
+  
+  
