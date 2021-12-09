56804cbe04679401f7557d563b549e9eaa18f7d6
Yue1Harriet1Huang <dariahuangster@gmail.com>
2021-12-09 16:49:51 -0500

    Update psql.py

--- data_pipelines/psql.py
+++ data_pipelines/psql.py
@@ -10,5 +10,20 @@ CONNECTION_STRING = os.getenv(CONNECTION_STRING)
 def pyodbc_query(query):
   cnxn = pyodbc.connect(CONNECTION_STRING)
   return(pd.read_sql(query, cnxn))
+
+def df_to_db(df, db):
+  cnxn = pyodbc.connect(CONNECTION_STRING)
+  cursor = cnxn.cursor()
+  columns = [c for c in df.columns.values]
+  n = len(columns)
+  values = ','.join(['?' for c in columns])
+  column = ','.join(columns)
+  sql = "INSERT INTO {0} ({1}) values({2})".format(db, columns, values)
+  rows = ','.join(['row.iloc[{}]'.format(i) for i, c in enumerate(columns)])
+  for index, row in df.iterrows():
+    command = 'cursor.execute("{0}", {1})'.format(sql, rows)
+    exec(command)
+  cnxn.commit()
+  cursor.close()
   
   
